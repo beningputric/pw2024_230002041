b11fc0667442bce4c95f08d150ff19db86c8ba30
BeningPutriCahyaniA <you@example.com>
2024-02-20 08:04:41 +0700

    Mengerjakan tugas1 sintaks php

---
+++ tugas1/tugas1sintaksphp.php
@@ -0,0 +1,21 @@
+<?php
+$topi = '"Topi saya Bundar,';
+$bundar = 'Bundar Topi saya."';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=, initial-scale=1.0">
+  <title>tugas1sintaksphp</title>
+</head>
+<body>
+  <h1>
+    <?php
+    echo "<em> $topi " . $bundar . "</em>" ;
+    ?>
+  </h1>
+  
+</body>
+</html>
---
+++ tugas1/tugas1sintaksphpb.php
@@ -0,0 +1,22 @@
+<?php
+// String untuk menyimpan banyak baris
+$baris = " Aku  adalah angka  <b>41</b><br>
+    Jika aku di kali 5, maka aku sekarang  <b>205</b><br>
+    jika aku di bagi 2, maka aku sekarang <b>102.5</b> <br>
+    jika aku di tambah 75, maka aku sekarang <b>177.5</b> <br>
+    jika dikurang 20, maka aku sekarang <b>157.5</b> ";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tugas1sintaksphpb</title>
+</head>
+<body>
+    <?php
+    echo $baris;
+    ?>
+</body>
+</html>
